f884fd5f38ab398d98a91464182e7fbf7499d907
Agusgomzok <agustin.gomez.ggg@gmail.com>
2025-03-26 21:57:52 -0300

    26/3/2025

---
+++ 01_Algoritmos/0.1.3.go
@@ -0,0 +1,17 @@
+//TOT := K*(DOS-Q)
+//D := DOS/K + Q * 2
+//E := TRUNC (Q*0,8 - DOS/3)
+//R := REDOND (Q/(3/K))
+//K := ABSO (K-Q) + 2 * DOS
+//RESULTADO := REDOND (ABSO((K-DOS)/Q)+TRUNC(1,5))
+Accion 0.1.3 es
+
+	AMBIENTE
+	K=4
+	Q=7
+	DOS=10
+	TOT:entero
+	PROCESO
+	TOT:=k*
+Fin Accion
+
---
+++ 01_Algoritmos/TEORIA.PY.py
@@ -0,0 +1,4 @@
+IVA = 21; #SUPER SENCIBLE A MAYUSCULAS; print "existe"; Print no "existe"
+print(IVA); #constante
+edad=20
+print (edad); #variable
